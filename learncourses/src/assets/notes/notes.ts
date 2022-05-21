6e8e471b7ab1f98bf72e2b7947dbbb0c0697a91d
benjadir <rida.jeba@gmail.com>
2022-05-21 20:19:33 +0100

    notes 1.2

---
+++ learncourses/src/assets/notes/notes.ts
@@ -0,0 +1,15 @@
+/**
+*-git add [file name|.]
+*-git reset head[file name|.]
+*-Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+*-git branch :all branches
+*-git push [remoteRepoName] [current branch name]
+*-git remote -v :origin  https://github.com/benjadir/WebEduca.git (fetch)
+origin  https://github.com/benjadir/WebEduca.git (push)
+*-git push origin master :take my
+
+
+*/
+
